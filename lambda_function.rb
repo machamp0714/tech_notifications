380c431aec3a3a6269bd13a215852a0c7a86008f
machamp0714 <rateliasudaisuki@gmail.com>
2020-10-10 15:31:08 +0900

    qiitaの記事をslackに通知

--- lambda_function.rb
+++ lambda_function.rb
@@ -4,12 +4,16 @@ require 'pry-byebug'
 require 'nokogiri'
 
 def lambda_handler(event:, context:)
-  slack_notification
+  urls = fetch_trend_from_qiita
+  text = <<~TEXT
+    #{urls.join("\n")}
+  TEXT
+  slack_notification(text)
 end
 
-def slack_notification
+def slack_notification(text)
   client = Slack::Web::Client.new(token: ENV['SLACK_TOKEN'])
-  client.chat_postMessage(channel: ENV['SLACK_CHANNEL'], text: 'hello, AWS Lambda!', as_user: true)
+  client.chat_postMessage(channel: ENV['SLACK_CHANNEL'], text: text, as_user: true)
 end
 
 # Qiitaからトレンド記事を取得する
